7afd6c538ca42f48262104738451a4ac8c9041fc
kirill povasin <povasinkirill@gmail.com>
2022-03-25 15:44:49 +0300

    дизайн и задачи функционал

--- js/aside.js
+++ js/aside.js
@@ -1,16 +1,20 @@
 const handlAsideHTML = document.querySelector("#handlAside")
 const asideHTML = document.querySelector("#aside")
 const mainHTML = document.getElementById("main")
-
+//закрывающая открывающая aside
 let showAside = true
 handlAsideHTML.addEventListener("click",()=>{
     if (showAside) {
         asideHTML.style.left = -15+"%" 
         mainHTML.style.left = 2+"%"
+        mainHTML.style.width = 96+"%"
+        handlAsideHTML.style.transform = 'rotate(' + 180 + 'deg)';
         showAside = !showAside
     } else{
         asideHTML.style.left = 0 
         mainHTML.style.left = 17+"%" 
+        mainHTML.style.width = 81+"%"
+        handlAsideHTML.style.transform = 'rotate(' + 0 + 'deg)';
         showAside = !showAside
     }
 })
--- pages/Tasks/js/search.js
+++ pages/Tasks/js/search.js
@@ -1,18 +1,23 @@
 const ManeTasks = ["попкин а. г.", "пупкин а. г.", "бобкин а. г.", "жопкин а. г." , "копкин а. г." ]
+const ManeText = ["купить арбузов и бананов и помидоров", "Подскажите, как в таком случае можно корректировать координаты, если всё вбито", "в строку Интересует замена 50 obj.style.transform", "попоп оппо окпо щощ пкр пшко пфоп пщш офуоп тищвжпш отфщкцп"]
 const searchHTML = document.querySelector("#search")
 const tasksHTML = document.querySelector(".tasks") 
+let tasks__text = document.querySelector("tasks__text")
 
 function render(mass) {
     tasksHTML.innerHTML = ""
     mass.forEach(task => {
-        tasksHTML.insertAdjacentHTML("beforeend", `<div class="tasks__item">
-        <p>${task}</p>
-        </div>`)
+        tasksHTML.insertAdjacentHTML("beforeend", ` <div class="tasks__item">
+        <p class="tasks__name">${task}</p>
+        <p class="tasks__text"> fwefweffwef</p>
+        <p class="tasks__date">21 марта</p>
+        <p class="openTasks">></p>
+    </div>`)
     });
 }
 
 render(ManeTasks)
-
+let tasks__item = document.querySelector(".tasks__item")
 searchHTML.addEventListener("input", ()=>{
     let filterTasks = ManeTasks.filter((task)=>{
         if (task.indexOf(searchHTML.value) != -1) {
@@ -23,3 +28,21 @@ searchHTML.addEventListener("input", ()=>{
     })
     render(filterTasks)
 })
+//надо проверить некоректно работает
+let openTasks = document.querySelector(".openTasks")
+let showTask = false
+tasksHTML.addEventListener("click",e=>{
+    // let clientHight = tasks__text.clientHeight
+    if (e.target.className = "openTasks") {
+        if (!showTask) {
+            openTasks.style.transform = 'rotate(' + 90 + 'deg)';
+            tasks__item.style.height = 200 + "px"
+            showTask = !showTask
+        } else{
+            tasks__item.style.height = 53 + "px"
+            openTasks.style.transform = 'rotate(' + 90 + 'deg)';
+            showTask = !showTask
+        }
+    }
+})
+

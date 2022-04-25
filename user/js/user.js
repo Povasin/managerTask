2742f9984add6e15fdfc7af69036fee8f8107b70
kirill povasin <povasinkirill@gmail.com>
2022-04-25 15:19:22 +0300

    оптимизация user.js не работает checkCount()

--- user/js/user.js
+++ user/js/user.js
@@ -1,21 +1,24 @@
-const openRedaction = document.getElementById("openRedaction")
+
 const redactionHTML = document.getElementById("redaction")
 const main__obert = document.querySelector(".main__obert")
-const surnameJS = document.querySelector(".surnameJS")
+let surnameJS = null
 const surnameHTML = document.querySelector("#surname")
 const nameHTML = document.querySelector("#name")
-const nameJS = document.querySelector("nameJS")
-const descriptionJS = document.querySelector('.descriptionJS')
+let nameJS = null
+let descriptionJS = null
 const discriptionSpan = document.getElementById("discriptionSpan")
-const errorHTML = document.querySelectorAll(".error")
-const birthdayJS = document.querySelector(".birthdayJS")
+let errorHTML = null
+let birthdayJS = null
 const birthday__date = document.querySelector(".birthday__date")
-const familyJS = document.querySelector("familyJS")
+let familyJS = null
 const family__text = document.querySelector(".family__text")
-const obertJS =document.querySelector(".obertJS")
+let obertJS = null
+let openRedaction = null
+
+
 
 //замена main
-redactionHTML.addEventListener("click", ()=>{
+redactionHTML.addEventListener("click", () => {
     main__obert.innerHTML = `<div class="obertJS">
     <div class="showRedact">
         <a href="./user.html" id="closeRedaction">X</a>
@@ -50,96 +53,71 @@ redactionHTML.addEventListener("click", ()=>{
         </select>
         <span class="error"></span>
     </div>
-</div>`
+    </div>`
+    openRedaction = document.getElementById("openRedaction")
+    surnameJS = document.querySelector(".surnameJS")
+    nameJS = document.querySelector(".nameJS")
+    descriptionJS = document.querySelector('.descriptionJS')
+    birthdayJS = document.querySelector(".birthdayJS")
+    errorHTML = document.querySelectorAll(".error")
+    familyJS = document.querySelector("familyJS")
+    obertJS = document.querySelector(".obertJS")
+    openRedaction.addEventListener("click", () => {
+        if (checkCount(surnameJS, surnameHTML, 0) == true && checkCount(nameJS, nameHTML, 1) == true && checkCount(descriptionJS, discriptionSpan, 2) == true && checkCount(birthdayJS, birthday__date, 3) == true && checkCount(familyJS, family__text, 4) == true) {
+            obertJS.innerHTML = `<div class="main__obert">
+            <div class="photo">
+                <div class="img__obert">
+                    <img src="../img/avatar.png">
+                </div>
+                <a href="#" id="redaction">редактировать</a>
+            </div>
+            <div class="information">
+                <div class="main__information">
+                        <p> <span id="surname">фамилия</span> <span id="name">Имя</span></p>
+                        <p class="discription">мини описание: <span id="discriptionSpan"></span></p>
+                        <p id="online">online</p>
+                </div>
+                <div class="information__name">
+                    <div class="birthday">
+                        <p>день рождение:</p>
+                        <p class="birthday__date">00.00.00</p>
+                    </div>
+                    <div class="family">
+                        <p>семейное положние:</p>
+                        <p class="family__text">не указано</p>
+                    </div>
+                    <div class="school">
+                        <p>образование:</p>
+                        <p class="school__text">не указано</p>
+                    </div>
+                </div>
+            </div>
+        </div>`
+        } else {
+            checkCount(surnameJS, surnameHTML, 0)
+            checkCount(nameJS, nameHTML, 1)
+            checkCount(descriptionJS, discriptionSpan, 2)
+            checkCount(birthdayJS, birthday__date, 3)
+            checkCount(familyJS, family__text, 4)
+        }
+    })
 })
 
-
-
-function checkSurname() {
-    console.log(check(surnameJS, 20));
-    if (surnameJS.value != "") {
-        surnameHTML.innerHTML = surnameJS.value 
-        return true
-    } else{
-        errorHTML[0].innerHTML = 'количество символов должно быть минимум 1'
-        return false
-    }
-}
-
-function chekName() {
-    if (nameJS.value != "") {
-        nameHTML.innerHTML = nameJS.value
-        return true
-    } else{
-        errorHTML[1].innerHTML = 'это поле не заполненно'
-        return false
-    }
-}
-function checkDiscription() {
-    if (descriptionJS.value != "") {
-        discriptionSpan.innerHTML = descriptionJS.value
-        return true
-    } else{
-        errorHTML[2].innerHTML = 'это поле не заполненно'
-        return false
-    }
-}
-function checkBirthday() {
-    if (birthdayJS.value != "") {
-        birthday__date.innerHTML = birthdayJS.value
+//проверка полей
+function checkCount(inputJS, inputHTML, index) {
+    //не может прочитать value
+    if (inputJS.value != "") {
+        inputHTML.innerHTML = inputJS.value
+        errorHTML[index].innerHTML = ''
         return true
     } else {
-        errorHTML[2].innerHTML = 'это поле не заполненно'
+        errorHTML[index].innerHTML = 'это поле не заполнено'
         return false
     }
 }
-function checkFamily() {
-    if (familyJS.value != "") {
-        family__text.innerHTML = familyJS.value
-        return true
-    }
-}
 
-// openRedaction.addEventListener("click", ()=>{
-//     if ( checkSurname()== true && chekName()==true && checkDiscription()==true && checkBirthday()==true &&  checkFamily()==true) {
-//         obertJS.innerHTML = `<div class="main__obert">
-//         <div class="photo">
-//             <div class="img__obert">
-//                 <img src="../img/avatar.png">
-//             </div>
-//             <a href="#" id="redaction">редактировать</a>
-//         </div>
-//         <div class="information">
-//             <div class="main__information">
-//                     <p> <span id="surname">фамилия</span> <span id="name">Имя</span></p>
-//                     <p class="discription">мини описание: <span id="discriptionSpan"></span></p>
-//                     <p id="online">online</p>
-//             </div>
-//             <div class="information__name">
-//                 <div class="birthday">
-//                     <p>день рождение:</p>
-//                     <p class="birthday__date">00.00.00</p>
-//                 </div>
-//                 <div class="family">
-//                     <p>семейное положние:</p>
-//                     <p class="family__text">не указано</p>
-//                 </div>
-//                 <div class="school">
-//                     <p>образование:</p>
-//                     <p class="school__text">не указано</p>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>`
-//     } else{
-//         checkSurname()
-//         chekName()
-//         checkDiscription()
-//         checkBirthday()
-//     }
-// })
 
-    console.log(openRedaction); 
-openRedaction.addEventListener("click", ()=>{
-    console.log(1);
-})
+
+
+
+
